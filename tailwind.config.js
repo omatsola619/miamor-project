158a9954f6a6fff3f66753521d75895aa64f7adf
omash <omarsunday0@gmail.com>
2025-08-05 10:22:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'first': '#084f4f',
+        'text': '#bce1e1',
+        'grayed': '#192626',
+        'h1': '#b3dada',
+        'ptext': '#8bb6b7',
+        'buttons': '#ffa100',
+        'view-more': '#ff8703',
+        '2h1': '#f2fafa',
+        'tik': '#0d5152,',
+        'tiktok2': '#898d96',
+        'wallet': '#1c615f',
+        'phone': '#133435',
+        'lightgray': '#d0e4e3',
+        'move': '#1c615e',
+        'imgs-text': '#898d96',
+        'two-phone': '#c27d73',
+        'footer': '#0d5152',
+        'sinput': '#3d5151',
+        'stext': '#736b67',
+        'register': '#e2b96d',
+        'sbg': '#001a1a',
+        'ibg': '#013534',
+      },
+    },
+  },
+  plugins: [],
+}
+
